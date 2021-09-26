6f9933f6e09db12e83a4dcc1aa6b849351b0c1f5
Alexander_Cardona_Herrera <alexcardonah90@gmail.com>
2021-09-26 17:53:55 -0500

    adding files for task 3, implementation of places

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+
+  $.getJSON('http://192.168.1.6:5001/api/v1/status/', function (data) {
+    if (data.status === 'OK') {
+      $('DIV#api_status').addClass('available');
+    } else {
+      $('DIV#api_status').removeClass('available');
+    }
+  });
+
+  $.ajax({
+    type: 'POST',
+    url: 'http://192.168.1.6:5001/api/v1/places_search',
+    data: JSON.stringify({}),
+    dataType: 'json',
+    contentType: 'application/json',
+    success: function (data) {
+      for (let dir of data) {
+        $('.places').append('<article>' + dir.name + '</article>');
+      };
+    }
+  });
+
+  const amenityStorage = {};
+
+  $('li :checkbox').change(function () {
+    let amenityId = $(this).attr('data-id');
+    let amenityName = $(this).attr('data-name');
+
+    if (this.checked) {
+      amenityStorage[amenityId] = amenityName;
+    } else {
+      delete amenityStorage[amenityId];
+    }
+
+    $('div.amenities h4').empty();
+    let newText = $.map(amenityStorage, function (x) {
+      return x;
+    }).join(', ');
+
+    $('div.amenities h4').text(newText);
+  });
+});
